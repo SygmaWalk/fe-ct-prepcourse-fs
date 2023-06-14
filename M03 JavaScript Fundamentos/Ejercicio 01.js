df78cea08d95b1a7a431692a342767879bfa1368
sygma <sygmawalk@gmail.com>
2023-06-13 22:25:58 -0300

    segundo intento

--- M03 JavaScript Fundamentos/Ejercicio 01.js
+++ M03 JavaScript Fundamentos/Ejercicio 01.js
@@ -5,10 +5,10 @@
 // por el correspondiente.
 
 // Crea una variable de tipo string.
-const nuevoString = null;
+const nuevoString = "Alejo";
 
 // Crea una variable de tipo number.
-const nuevoNumero = null;
+const nuevoNumero = 12;
 
 // Crea una variable de tipo boolean.
 const nuevoBoolean = null;
